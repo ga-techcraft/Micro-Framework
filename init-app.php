9e0e4f410fbd9afbefecb720513c4bee8de9fec6
Tech Craft <ga.techcraft@gmail.com>
2025-03-04 19:44:30 +0900

    feat: Create script to create cars table

--- init-app.php
+++ init-app.php
@@ -6,22 +6,29 @@ spl_autoload_register(function($class) {
     if (file_exists(stream_resolve_include_path($file))) include($file);
 });
 
-use Helpers\Settings;
 use Database\MySQLWrapper;
-/*
- * https://www.php.net/manual/en/class.mysqli.php で利用可能なすべてのメソッドを確認できます。
- */
-$mysqli = new MySQLWrapper('localhost', Settings::env('DATABASE_USER'), Settings::env('DATABASE_USER_PASSWORD'), Settings::env('DATABASE_NAME'));
 
-// https://www.php.net/manual/en/mysqli.get-charset.php
+// getoptはCLIで渡された指定された引数のオプションです。値のペアの配列を返します。
+// 値が渡されない場合 (例：--myArg=123、値は123) は、値はfalseになります。issetを使用してそれが存在するかどうかをチェックします。
+// short_optionsは、短いオプションの文字の配列を表す文字列を取り入れます。例えばabcは -a -b -c のオプションをチェックします。ロングオプションはオプションの完全な名前です。
+$opts = getopt('',['migrate']);
+if(isset($opts['migrate'])){
+    printf('Database migration enabled.');
+    // includeはPHPファイルをインクルードして実行します
+    include('Database/setup.php');
+    printf('Database migration ended.');
+}
+
+$mysqli = new MySQLWrapper();
+
 $charset = $mysqli->get_charset();
 
 if($charset === null) throw new Exception('Charset could be read');
 
-// データベースの文字セット、照合順序、統計情報について取得します。
+// データベースの文字セット、照合順序、および統計に関する情報を取得します。
 printf(
     "%s's charset: %s.%s",
-    Settings::env('DATABASE_NAME'),
+    $mysqli->getDatabaseName(),
     $charset->charset,
     PHP_EOL
 );
@@ -32,5 +39,5 @@ printf(
     PHP_EOL
 );
 
-// 接続を閉じるには、closeメソッドを使用します。
+// 接続を閉じるには、closeメソッドが使用されます。
 $mysqli->close();
