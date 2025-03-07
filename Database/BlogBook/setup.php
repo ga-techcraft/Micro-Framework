0adae4915ab489df849e6783cea8828b0ed4713a
Tech Craft <ga.techcraft@gmail.com>
2025-03-07 11:39:58 +0900

    feat: create script to create user_settings, categories, tags, and post_tag tables

--- Database/BlogBook/setup.php
+++ Database/BlogBook/setup.php
@@ -22,6 +22,9 @@ try {
     runSqlQuery($mysqli, __DIR__ . '/post-like-setup.sql', 'post_like');
     runSqlQuery($mysqli, __DIR__ . '/comments-setup.sql', 'comments');
     runSqlQuery($mysqli, __DIR__ . '/comment-like-setup.sql', 'comment_like');
+    runSqlQuery($mysqli, __DIR__ . '/user-settings-setup.sql', 'user_settings');
+    runSqlQuery($mysqli, __DIR__ . '/tags-setup.sql', 'tags');
+    runSqlQuery($mysqli, __DIR__ . '/post-tag-setup.sql', 'post_tag');
 
 } catch (Exception $e) {
     print "Error: " . $e->getMessage() . PHP_EOL;
