d9be15ac23af09f5e93b9076d1da0a5748bd1eb1
Tech Craft <ga.techcraft@gmail.com>
2025-03-25 15:12:58 +0900

    feat: Complete Web API to return computer parts data

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // 開発用、何でも許可
 
 spl_autoload_extensions(".php");
 spl_autoload_register(function($class) {
@@ -6,30 +7,42 @@ spl_autoload_register(function($class) {
     if (file_exists(stream_resolve_include_path($file))) include($file);
 });
 
-// ルートをロードします。
+
+$DEBUG = true;
+
+// ルートを読み込みます。
 $routes = include('Routing/routes.php');
 
 // リクエストURIを解析してパスだけを取得します。
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = ltrim($path, '/');
 
-// ルートにパスが存在するかチェックします。
+// ルートにパスが存在するかチェックする
 if (isset($routes[$path])) {
-    $view = $routes[$path];
-    $viewPath = sprintf("%s/Views/%s.php",__DIR__, $view);
+    // コールバックを呼び出してrendererを作成します。
+    $renderer = $routes[$path]();
 
-    if (file_exists($viewPath)) {
+    try{
         // ヘッダーを設定します。
-        include 'Views/layout/header.php';
-        include $viewPath;
-        include 'Views/layout/footer.php';
-    } else {
+        foreach ($renderer->getFields() as $name => $value) {
+          // 改行コードがあるか確認 → セキュリティ対策（ヘッダーインジェクション防止）
+          if (preg_match('/[\r\n]/', $value)) {
+              http_response_code(500);
+              if ($DEBUG) print("Invalid header value: '$value'");
+              exit;
+          }
+      
+          header("$name: $value");
+          print($renderer->getContent());
+        }
+    }
+    catch (Exception $e){
         http_response_code(500);
-        printf("<br>debug info:<br>%s<br>%s", "Internal error, please contact the admin.");
+        print("Internal error, please contact the admin.<br>");
+        if($DEBUG) print($e->getMessage());
     }
 } else {
-    // 一致するルートがない場合、404エラーを表示する
+    // マッチするルートがない場合、404エラーを表示します。
     http_response_code(404);
     echo "404 Not Found: The requested route was not found on this server.";
-    printf("<br>debug info:<br>%s<br>%s", json_encode($routes),$path);
 }
