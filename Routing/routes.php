239a3eff3637db1221892441a6b06180b5878546
Tech Craft <ga.techcraft@gmail.com>
2025-03-25 17:12:50 +0900

    feat: Create DAO

--- Routing/routes.php
+++ Routing/routes.php
@@ -6,19 +6,27 @@ use Helpers\ValidationHelper;
 use Response\HTTPRenderer;
 use Response\Render\HTMLRenderer;
 use Response\Render\JSONRenderer;
+use Database\DataAccess\Implementations\ComputerPartDAOImpl;
 
 return [
-    'random/part'=>function(): HTTPRenderer{
-        $part = DatabaseHelper::getRandomComputerPart();
+      'random/part'=>function(): HTTPRenderer{
+        $partDao = new ComputerPartDAOImpl();
+        $part = $partDao->getRandom();
 
-        return new HTMLRenderer('component/part', ['part'=>$part]);
+        if($part === null) throw new Exception('No parts are available!');
+
+        return new HTMLRenderer('component/computer-part-card', ['part'=>$part]);
     },
     'parts'=>function(): HTTPRenderer{
         // IDの検証
         $id = ValidationHelper::integer($_GET['id']??null);
 
-        $part = DatabaseHelper::getComputerPartById($id);
-        return new HTMLRenderer('component/part', ['part'=>$part]);
+        $partDao = new ComputerPartDAOImpl();
+        $part = $partDao->getById($id);
+
+        if($part === null) throw new Exception('Specified part was not found!');
+
+        return new HTMLRenderer('component/computer-part-card', ['part'=>$part]);
     },
     'api/random/part'=>function(): HTTPRenderer{
         $part = DatabaseHelper::getRandomComputerPart();
