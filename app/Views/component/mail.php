b8040b0f24315b1686cb9bed9a9dcb3e217d90e5
Tech Craft <ga.techcraft@gmail.com>
2025-05-27 17:59:51 +0900

    feat: Implement email system

---
+++ app/Views/component/mail.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Micro-Frame-App</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h1>Micro-Frame-App</h1>
+
+    <p>こんにちは、Micro-Frame-AppのHTML形式のテストメールです。</p>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Micro-Frame-App</p>
+    </footer>
+    </body>
+</html>
---
+++ postfix/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+service postfix start
+tail -F /var/log/mail.log
